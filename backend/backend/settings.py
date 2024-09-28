6a0497f122666c4c1ebb8ff542d8edff85c0f528
khushpokar <khushpokar8@gmail.com>
2024-09-28 18:46:50 +0530

    done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4!^hf&(c+14(2uq(*nm%fb(fd54zttx)69(*7lg7i%9e!zh*&'
+SECRET_KEY = config('S_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -109,7 +110,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'khushpatel4848@gmail.com'
-EMAIL_HOST_PASSWORD = 'nohe sisp lqfq mxdr'
+EMAIL_HOST_PASSWORD = config('E_key')
 
 
 # Internationalization
